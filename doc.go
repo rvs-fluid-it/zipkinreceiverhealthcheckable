dbc100372b0d73ec2c4d814887415bd95bfeb13f
Koen Roevens <koen.roevens@cumulus.uzleuven.be>
2025-02-03 07:42:24 +0100

    Initial commit

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate mdatagen metadata.yaml
+
+// Package zipkinreceiverhealthcheckable receives Zipkin traces and has an healthckeck provided.
+package zipkinreceiverhealthcheckable // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/zipkinreceiverhealthcheckable"
